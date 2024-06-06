76c06f7b02ad95928198b5aa2e1a2883b968d6c1
Badr Annabi <badrannabi@Badrs-MacBook-Pro.local>
2024-06-06 13:07:30 +0100

    TASK 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Writing a type-annotated function """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function a string str1 and a string str2
+    as arguments and returns a concatenated string """
+    return str1 + str2

ada33fe1878e4a655819ffb6ca9dd29e9e01429b
Badr Annabi <badrannabi@Badrs-MacBook-Pro.local>
2024-06-10 13:59:58 +0100

    TASK 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """The basics of async"""
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     tasks = [wait_random(max_delay) for _ in range(n)]
     delay_list = await asyncio.gather(*tasks)
     return sorted(delay_list)
